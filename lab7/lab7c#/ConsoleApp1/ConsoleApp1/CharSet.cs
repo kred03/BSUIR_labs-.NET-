dcfde5cdad2623ab86b6a002524b6f81f373f413
Dzmitry Danilau <dimad7549@gmail.com>
2024-05-07 08:18:33 +0300

    add lab7
    add lab

---
+++ lab7/lab7c#/ConsoleApp1/ConsoleApp1/CharSet.cs
@@ -0,0 +1,72 @@
+﻿public class CharSet
+{
+    private char[] _chars;
+
+    public CharSet(char[] chars)
+    {
+        if (chars.Distinct().Count() != chars.Length)
+            throw new ArgumentException("Множество символов не должно содержать повторяющихся символов.");
+        _chars = chars;
+    }
+
+    public char this[int index]
+    {
+        get { return _chars[index]; }
+        set
+        {
+            if (_chars.Contains(value))
+                throw new ArgumentException("Этот символ уже присутствует в множестве.");
+            _chars[index] = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return new string(_chars);
+    }
+
+    public static CharSet operator +(CharSet a, CharSet b)
+    {
+        return new CharSet(a._chars.Union(b._chars).ToArray());
+    }
+
+    public static CharSet operator -(CharSet a, CharSet b)
+    {
+        return new CharSet(a._chars.Except(b._chars).ToArray());
+    }
+
+    public static CharSet operator *(CharSet a, CharSet b)
+    {
+        return new CharSet(a._chars.Intersect(b._chars).ToArray());
+    }
+
+    public static bool operator ==(CharSet a, CharSet b)
+    {
+        return a._chars.SequenceEqual(b._chars);
+    }
+
+    public static bool operator !=(CharSet a, CharSet b)
+    {
+        return !(a == b);
+    }
+
+    public static bool operator true(CharSet a)
+    {
+        return a._chars.Length > 0;
+    }
+
+    public static bool operator false(CharSet a)
+    {
+        return a._chars.Length == 0;
+    }
+
+    public static implicit operator string(CharSet a)
+    {
+        return a.ToString();
+    }
+
+    public static explicit operator CharSet(string s)
+    {
+        return new CharSet(s.ToCharArray());
+    }
+}
---
+++ lab7/lab7c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        CharSet set1 = new CharSet(new char[] { 'a', 'b', 'c' });
+        CharSet set2 = new CharSet(new char[] { 'b', 'c', 'd' });
+
+        Console.WriteLine("Set1: " + set1);
+        Console.WriteLine("Set2: " + set2);
+
+        CharSet union = set1 + set2;
+        Console.WriteLine("Union: " + union);
+
+        CharSet difference = set1 - set2;
+        Console.WriteLine("Difference: " + difference);
+
+        CharSet intersection = set1 * set2;
+        Console.WriteLine("Intersection: " + intersection);
+
+        Console.WriteLine("Set1 == Set2: " + (set1 == set2));
+        Console.WriteLine("Set1 != Set2: " + (set1 != set2));
+
+        string set1AsString = set1;
+        Console.WriteLine("Set1 as string: " + set1AsString);
+
+        CharSet set3 = (CharSet)"efg";
+        Console.WriteLine("Set3: " + set3);
+    }
+}
